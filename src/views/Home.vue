bec910a5973907e437cd25ea18a48bd7e2e19b6b
Wu Sai <wusai.ustc@outlook.com>
2023-03-05 13:35:55 +0800

    解决element pius导入问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <p>测试vue-router</p>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <script>
